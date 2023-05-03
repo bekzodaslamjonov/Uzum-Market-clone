063b1ad729e38b918c12b2289a677a3431936a5e
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-05-03 09:19:54 +0500

    inputphone

--- src/Components/Common/Authentication/LogIn.js
+++ src/Components/Common/Authentication/LogIn.js
@@ -1,6 +1,7 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import InputMask from "react-input-mask";
 
 export default function LogIn({ closLog }) {
   return (
@@ -53,6 +54,7 @@ export default function LogIn({ closLog }) {
             gap: "20px",
           }}
         >
+          <Typography sx={{ fontSize: "20px" }}>Регистрация</Typography>
           <TextField
             id="outlined-basic"
             label="Электронная почта"
@@ -60,13 +62,36 @@ export default function LogIn({ closLog }) {
             type="email"
             sx={{ width: "100%" }}
           ></TextField>
-          <TextField
-            id="outlined-basic"
-            label="Введите номер телефона"
-            variant="outlined"
-            type="telephone "
-            sx={{ width: "100%" }}
-          ></TextField>
+          <InputMask
+            mask="+999 (99) 999-99-99"
+            maskChar=" "
+            // value={props.value}
+            // onChange={props.onChange}
+          >
+            {() => (
+              <TextField
+                label="Введите номер телефона"
+                variant="outlined"
+                fullWidth
+              />
+            )}
+          </InputMask>
+          <Button
+            sx={{
+              width: "100%",
+              height: "50px",
+              backgroundColor: "#7000ff",
+              ":hover": { backgroundColor: "#7000ff" },
+            }}
+          >
+            <Typography sx={{ color: "white" }}>Зарегистрироваться</Typography>
+          </Button>
+          <Typography sx={{ fontSize: "14px" }}>
+            Eсть аккаунт ?{" "}
+            <Link style={{ textDecoration: "none", color: "blue" }}>
+              Подключится
+            </Link>
+          </Typography>
         </Box>
         <Box sx={{ textAlign: "center" }}>
           <Typography>
