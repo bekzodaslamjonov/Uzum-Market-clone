fd317f356b0ffa7fd2c84916dcb9bb15342f80bd
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-05-07 17:34:47 +0500

    new

--- src/Components/Common/Navbar/TopDrawer.js
+++ src/Components/Common/Navbar/TopDrawer.js
@@ -1,15 +1,17 @@
-import { Box, Divider, Drawer, List, ListItem, ListItemButton, Typography } from "@mui/material";
+import { Box, List, ListItem, ListItemButton, Typography } from "@mui/material";
 import React, { useState } from "react";
 
 export default function TopDrawer({ show }) {
   let [transform1] = useState("translateY(-120vh)");
   let [transform] = useState("translateY(0vh)");
-  let [draw,setDraw]=useState(false)
-  let [draw1,setDraw1]=useState(true)
+  // let [draw, setDraw] = useState(false);
+  let [electro, setElektro] = useState("none");
+  let [technic,setTechnic]=useState('none')
+  // let [draw1,setDraw1]=useState(true)
 
-  const open =()=>{
-    setDraw(!draw)
-  }
+  // const open = () => {
+  //   setDraw(!draw);
+  // };
   return (
     <>
       <Box
@@ -27,26 +29,57 @@ export default function TopDrawer({ show }) {
           justifyContent: "center",
         }}
       >
-        <Box sx={{ width: "80%", height: "100%", display: "flex",justifyContent:'center' }}>
-          <Box sx={{ width: "270px", height: "100%",backgroundColor:'aqua' }}>
+        <Box
+          sx={{
+            width: "80%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box sx={{ width: "270px", height: "100%", backgroundColor: "aqua" }}>
             <List>
-              <ListItem >
-                <ListItemButton sx={{":hover":`${()=>open}`}}>
-
-                <Typography>
-                  hello
-                </Typography>
+              <ListItem>
+                <ListItemButton
+                  onMouseOver={() => setElektro("block")}
+                  // onMouseLeave={() => setElektro("none")}
+                >
+                  <Typography>hello</Typography>
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton
+                  onMouseOver={() => setTechnic("block") }
+                  // onMouseLeave={() => setTechnic("none")}
+                >
+                  <Typography>technic</Typography>
                 </ListItemButton>
               </ListItem>
             </List>
           </Box>
-          <Box sx={{width:'950px',height:'100%',backgroundColor:'red' }}>
-            
-      
+          <Box sx={{ width: "950px", height: "100%" }}>
+            <Box
+              sx={{
+                width: "950px",
+                height: "100%",
+                display: electro,
+                backgroundColor: "red",
+              }}
+            >
+              electro
+            </Box>
+            <Box
+              sx={{
+                width: "950px",
+                height: "100%",
+                display: technic,
+                backgroundColor: "red",
+              }}
+            >
+              technic
+            </Box>
           </Box>
         </Box>
-   
-
       </Box>
     </>
   );
