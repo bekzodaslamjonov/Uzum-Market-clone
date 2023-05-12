a9404285e4ac92cecb62815da29e97868a1d2831
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-05-12 20:29:56 +0500

    new

--- src/Components/Common/Navbar/Categories.js
+++ src/Components/Common/Navbar/Categories.js
@@ -5,28 +5,31 @@ import { Api } from "../../../Api/Api";
 import { Link } from "react-router-dom";
 
 export default function Categories({ show }) {
-  var [categori, setCategori] = useState([]);
-  var [categoriS, setCategoriS] = useState([]);
-  var [filtered, setFiltered] = useState([]);
   var [category, setCategory] = useState([]);
+  var [FilterC, setFilterC] = useState([]);
+  var [FilterCa, setFilterCa] = useState([]);
+  var [filtered, setFiltered] = useState([]);
+  var [theme, settheme] = useState([]);
+  var [themeC,setThemeC]=useState([])
   let [transform1] = useState("translateY(-150vh)");
   let [transform] = useState("translateY(0vh)");
-  // useEffect(() => {
-  //   axios.get(Api + "category").then((res) => setCategori(res.data));
-  // }, []);
-  // useEffect(() => {
-  //   axios.get(Api + "categoriesSmall").then((res) => setCategoriS(res.data));
-  // }, []);
+  useEffect(() => {
+    axios.get(Api + "category").then((res) => setCategory(res.data));
+  }, []);
+  useEffect(() => {
+    axios.get(Api + "themes").then((res) => settheme(res.data));
+  }, []);
+  useEffect(() => {
+    axios.get(Api + "FilterC").then((res) => setFilterC(res.data));
+  }, []);
   var mouseFunc = (index) => {
-    filtered = categoriS.filter(
-      (item) => item.categoryId === categori[index].id
-    );
+    filtered = category.filter((item) => item.id === category[index].compare);
     setFiltered(filtered);
-    category = filtered[0].theme;
-    setCategory(category);
+    themeC =theme.filter((item)=>item.compare ===filtered[0].compare)
+    setThemeC(themeC)
+    FilterCa=FilterC.filter((item)=>item.compare ===filtered[0].compare )
+    setFilterCa(FilterCa)
   };
-  //  console.log(filtered[0].theme);
-
   return (
     <Box
       sx={{
@@ -52,8 +55,8 @@ export default function Categories({ show }) {
       >
         <Box sx={{ width: "25%", height: "100%" }}>
           <List>
-            {categori.length !== 0
-              ? categori.map((item, index) => (
+            {category.length !== 0
+              ? category.map((item, index) => (
                   <Link
                     key={index}
                     to={item.link}
@@ -67,10 +70,10 @@ export default function Categories({ show }) {
                           gap: "10px",
                         }}
                       >
-                        <i
+                        {/* <i
                           style={{ fontSize: "20px" }}
                           className={item.icons}
-                        ></i>
+                        ></i> */}
                         <Typography>{item.name}</Typography>
                       </Box>
                     </ListItemButton>
@@ -86,307 +89,12 @@ export default function Categories({ show }) {
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            flexDirection:'column'
+            flexDirection: "column",
           }}
         >
           {filtered.length !== 0
-            ? filtered
-                .filter((item) => item.id === filtered[0].id)
-                .map((item, index) => (
-                  // <Box
-                  //   key={index}
-                  //   sx={{
-                  //     width: "100%",
-                  //     height: "100%",
-                  //     display: "flex",
-                  //     justifyContent: "center",
-                  //     flexDirection: "column",
-                  //     alignItems: "center",
-                  //   }}
-                  // >
-                  //   <Box sx={{ width: "95%", height: "5%" }}>
-                  //     <Typography sx={{ fontSize: "18px" }}>
-                  //       {item.name}
-                  //     </Typography>
-                  //   </Box>
-                  //   <Box
-                  //     sx={{
-                  //       width: "95%",
-                  //       height: "90%",
-                  //       display: "flex",
-                  //       justifyContent: "space-between",
-                  //     }}
-                  //   >
-                  //     <Box sx={{ width: "30%", height: "auto" }}>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[0].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[0].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[1].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[1].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[2].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[3].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[2].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[4].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[5].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //     </Box>
-                  //     <Box sx={{ width: "30%", height: "auto" }}>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[3].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[6].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[7].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[4].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[8].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[9].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[5].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[10].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[11].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //     </Box>
-                  //     <Box sx={{ width: "30%", height: "auto" }}>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[6].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[12].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[13].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[7].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[14].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[15].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //       <Box
-                  //         sx={{
-                  //           width: "100%",
-                  //           height: "35%",
-                  //           display: "flex",
-                  //           flexDirection: "column",
-                  //           gap: "8px",
-                  //         }}
-                  //       >
-                  //         <Box>
-                  //           <Typography sx={{ fontSize: "18px" }}>
-                  //             {item.theme[8].name}
-                  //           </Typography>
-                  //         </Box>
-                  //         <Box
-                  //           sx={{
-                  //             display: "flex",
-                  //             flexDirection: "column",
-                  //             gap: "3px",
-                  //           }}
-                  //         >
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[16].name}
-                  //           </Typography>
-                  //           <Typography variant="p" sx={{ fontSize: "14px" }}>
-                  //             {item.small[1].name}
-                  //           </Typography>
-                  //         </Box>
-                  //       </Box>
-                  //     </Box>
-                  //   </Box>
-                  // </Box>
-                  <>
+            ? filtered.map((item, index) => (
+                <>
                   <Typography>{item.name}</Typography>
                   <Box
                     sx={{
@@ -397,16 +105,32 @@ export default function Categories({ show }) {
                       flexWrap: "wrap",
                     }}
                   >
-                    {category !== 0
-                      ? category.map((item, index) => (
-                        <Box sx={{width:'30%',height:'100px',backgroundColor:"aqua"}}>
-                          <Typography>{item.name}</Typography>
-                        </Box>
+                    {themeC.length !== 0
+                      ? themeC.map((item, index) => (
+                          <Box
+                            sx={{
+                              width: "auto",
+                              height: "100px",
+                              backgroundColor: "aqua",
+                            }}
+                          >
+                            <Typography>{item.name}</Typography>
+                            {
+                              FilterCa.length !== 0
+                             ? FilterCa.map((item,index)=>(
+                                  <Box>
+                                    <Typography>
+                                      {item.name}
+                                    </Typography>
+                                  </Box>
+                              )):""
+                            }
+                          </Box>
                         ))
                       : ""}
                   </Box>
-                  </>
-                ))
+                </>
+              ))
             : ""}
         </Box>
       </Box>
